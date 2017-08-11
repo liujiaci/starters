4ed9e8a8cae5f861581e5406c994785c9845170c
Jiaci LIU <Ghost@Jiacis-MacBook-Air.local>
2017-08-11 14:55:26 +0200

    setup.py has been added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # starters
-this is a project for starters on GitHub 
+this is a project for starters on GitHub
+git tutorials 
---
+++ setup.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='starters',
+    version='0.1',
+    description='',
+    author='Jiaci LIU',
+    author_email='jiaci.liu@gmail.com',
+    packages=['starters'],
+)
